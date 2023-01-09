73967e2ee2768a1e63d9d292e61c68a4f8332a44
yumeow0122 <yumeow0122@gmail.com>
2023-01-09 17:38:26 +0800

    123321

--- Senior/OperatingSystem/runSource.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf source/out*
-rm main
-docker run -it --rm -v $PWD:/tmp -w /tmp gcc /bin/bash -c "gcc -lpthread -o main source.c; ./main"
--- Senior/OperatingSystem/runThread.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf thread/out*
-rm main
-docker run -it --rm -v $PWD:/tmp -w /tmp gcc /bin/bash -c "gcc -lpthread -o main thread.c; ./main"
