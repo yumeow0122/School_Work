00786082b61bfec34ccf46f21ed8ba4cc506f4e9
yumeow0122 <yumeow0122@gmail.com>
2023-03-10 15:55:01 +0800

    Network Programming: * Initial home work invironment

---
+++ Junior/NetworkProgramming/HW1/obj/shell.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <cstring>
+
+char coommand[5000];
+
+void input(){
+    scanf("%c", command);
+}
+
+int main()
+{
+    while(1){
+        input();
+        printf("%s", char);
+    }
+    return 0;
+}
