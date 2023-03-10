4775f4362764fccc1d0308addb267e7fe0d18539
yumeow0122 <yumeow0122@gmail.com>
2023-03-11 01:12:57 +0800

    Network Programming HW1: * Move out I/O part to "IOController"

--- Junior/NetworkProgramming/HW1/src/shell.c
+++ Junior/NetworkProgramming/HW1/src/shell.c
@@ -49,45 +49,6 @@ void end()
     printf("----- Shell End. -----\n");
 }
 
-// // get user command and return the status code of command
-// int input(char *command)
-// {
-//     printf("%% ");
-//     fgets(command, 5000, stdin);
-//     if (strcmp(command, "exit\n") == 0 || strcmp(command, "quit\n") == 0)
-//     {
-//         return -1; // exit code
-//     }
-//     return 1;
-// }
-
-// int command_parse(char *command, char **args)
-// {
-//     int argc = 0;
-//     char *arg = malloc(COMMAND_SIZE * sizeof(char));
-//     char *delim = " \n";
-
-//     arg = strtok(command, delim);
-//     while (arg != NULL)
-//     {
-//         args[argc++] = arg;
-//         arg = strtok(NULL, delim);
-//     }
-//     args[argc] = NULL;
-//     return argc;
-// }
-
-int get_mode(int argc, char **args)
-{
-    if (argc > 1)
-    {
-        for (int i = 0; i < argc; i++)
-            if (strcmp(args[i], "|") == 0)
-                return 0;
-    }
-    return 1;
-}
-
 void run_external(int argc, char **args)
 {
     pid_t pid, wpid;
