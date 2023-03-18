dc6c83511ff7d42bfb73e568c55badc25ec39f95
yumeow0122 <yumeow0122@gmail.com>
2023-03-19 01:51:46 +0800

    Network Programming HW1: * steal Accco's code QQ

---
+++ Junior/NetworkProgramming/Network-Programming-Homework/include/runCommand.c
@@ -0,0 +1,113 @@
+#include <ctype.h>
+
+#define READ_END 0
+#define WRITE_END 1
+int save_in;
+
+/**
+ * @brief run the command
+ * There are three processes in this function which share 2 pipe.
+ * The first will fork to a child process to run the command which read the stdin from second child and produce stdout to parent.
+ * The second process is also the child process, that generate stdin for first child process.
+ * The last process is the parent process, it will read the stdout from the first child process.
+ *
+ * link1: write echo output to command stdin  (child2 -> child1)
+ * link2: read command output to parent stdin (child1 -> parent)
+ *
+ * @param command command to run
+ */
+int run_command(const char *command)
+{
+    int save_in;
+    save_in = dup(STDIN_FILENO);
+
+    // copy previousCommandOutput to currentCommandOutput and clear previousCommandOutput
+    char currentCommandOutput[PREVIOUS_COMMAND_OUTPUT_SIZE];
+    memset(currentCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+    strcat(currentCommandOutput, previousCommandOutput);
+    memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+
+    int link1[2];
+    int link2[2];
+    pid_t pid1, pid2;
+    char tempBuffer[4096 + 1];
+    char *cpcommand = malloc(MAX_COMMANDS_SIZE * sizeof(char));
+    char **_command = malloc(MAX_COMMANDS_SIZE * sizeof(char *));
+
+    cpcommand = strdup(command);
+    command_parse(cpcommand, _command);
+
+    if (pipe(link1) == -1)
+        die("pipe1");
+    if (pipe(link2) == -1)
+        die("pipe2");
+
+    if ((pid1 = fork()) == -1)
+        die("fork1");
+
+    if (pid1 == 0)
+    {
+        // child 1
+
+        // set I/O to link1 and link2
+        dup2(link1[READ_END], STDIN_FILENO);
+        dup2(link2[WRITE_END], STDOUT_FILENO);
+
+        // close communcation between link1 and link2
+        close(link1[WRITE_END]);
+        close(link2[READ_END]);
+
+        execvp(_command[0], _command);
+        char *error = malloc(128 * sizeof(char));
+        strcpy(error, _command[0]);
+        strcat(error, ": command not found");
+        die(error);
+    }
+    else
+    {
+        if ((pid2 = fork()) == -1)
+            die("fork2");
+        if (pid2 == 0)
+        {
+            // child
+            dup2(link1[WRITE_END], STDOUT_FILENO);
+
+            close(link1[READ_END]);
+            close(link2[READ_END]);
+            close(link2[WRITE_END]);
+
+            char *argv[] = {"./bin/echo", "-e", currentCommandOutput, NULL};
+            execvp(argv[0], argv);
+            die("echo: command not found");
+        }
+        else
+        {
+            // parent
+            dup2(link2[READ_END], STDIN_FILENO);
+
+            close(link1[READ_END]);
+            close(link1[WRITE_END]);
+            close(link2[WRITE_END]);
+
+            int nbytes = 0;
+            while (0 != (nbytes = read(link2[READ_END], tempBuffer, sizeof(tempBuffer))))
+            {
+                memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+                strcat(previousCommandOutput, tempBuffer);
+                memset(tempBuffer, 0, 4096);
+            }
+            // remove the last newline character if it exists
+            size_t len = strlen(previousCommandOutput);
+            if (len > 0 && previousCommandOutput[len - 1] == '\n')
+                previousCommandOutput[len - 1] = '\0';
+
+            // wait for child processes to finish
+            wait(NULL);
+        }
+        // wait for child processes to finish
+        wait(NULL);
+    }
+    // return to the original stdin for parent process
+    dup2(save_in, STDIN_FILENO);
+    free(cpcommand);
+}
---
+++ Junior/NetworkProgramming/Network-Programming-Homework/src/main.c
@@ -0,0 +1,196 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <dirent.h>
+#include <ctype.h>
+
+#define PREVIOUS_COMMAND_OUTPUT_SIZE 32768
+#define die(e)                      \
+    do                              \
+    {                               \
+        fprintf(stderr, "%s\n", e); \
+        exit(EXIT_FAILURE);         \
+    } while (0);
+
+char previousCommandOutput[PREVIOUS_COMMAND_OUTPUT_SIZE];
+
+// use for number pipe
+char savedCommandOutput[PREVIOUS_COMMAND_OUTPUT_SIZE];
+
+#include "../include/commandParse.c"
+#include "../include/runCommand.c"
+
+void init()
+{
+    printf("Welcome to the shell!\n");
+    printf("Type 'quit' or 'exit' to exit the shell.\n");
+
+    memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+
+    // mark this line if you want to use the system executables.
+    setenv("PATH", "/bin:./bin", 1);
+}
+
+int main(int argc, char **argv, char **envp)
+{
+    char command[MAX_COMMANDS_SIZE];
+    char quit[] = "quit";
+    char _exit[] = "exit";
+    char _setenv[] = "setenv";
+    char printenv[] = "printenv";
+
+    int pipeCounter = 0;
+
+    init();
+    while (1)
+    {
+        char **splitedCommand = malloc(MAX_COMMANDS_SIZE * sizeof(char *));
+        int piped = 0;
+        printf("%% ");
+
+        if (fgets(command, MAX_COMMANDS_SIZE, stdin) == NULL)
+        {
+            printf("\n");
+            break;
+        }
+
+        int splitedCommandCount = command_parse(command, splitedCommand);
+        if (splitedCommandCount == 0)
+            continue;
+
+        int currentCommandIndex = 0;
+        if (strcmp(splitedCommand[0], quit) == 0 || strcmp(splitedCommand[0], _exit) == 0)
+            break;
+
+        /**
+         * Because the setenv in child process will not affect the parent process,
+         * so setenv can't be implemented by directly calling run_bash.
+         *
+         * And printenv can't be implemented by directly calling either,  because we override the PATH,
+         * causing printenv in system executables not found.
+         *
+         * printenv and setenv only work when it is at the beginning of the command, but support pipe.
+         *
+         * Save the output of printenv to previousCommandOutput, so that it can be used in the next command.
+         */
+        if (strcmp(splitedCommand[0], printenv) == 0)
+        {
+            if (splitedCommandCount < 2 || strcmp(splitedCommand[1], "|") == 0)
+            {
+                currentCommandIndex += 2;
+                memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+                for (char **env = envp; *env != 0; env++)
+                {
+                    char *thisEnv = *env;
+                    strcat(previousCommandOutput, thisEnv);
+                    strcat(previousCommandOutput, "\n");
+                }
+            }
+            else if (splitedCommandCount > 2 && strcmp(splitedCommand[2], "|") != 0)
+            {
+                printf("printenv: too many arguments\n");
+                memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+                continue;
+            }
+            else
+            {
+                currentCommandIndex += 3;
+                memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+                strcat(previousCommandOutput, getenv(splitedCommand[1]));
+            }
+
+            if (strcmp(&previousCommandOutput[strlen(previousCommandOutput) - 1], "\n") == 0)
+                previousCommandOutput[strlen(previousCommandOutput) - 1] = '\0';
+        }
+        else if (strcmp(splitedCommand[0], _setenv) == 0)
+        {
+            if (splitedCommandCount < 3)
+                printf("setenv: not enough arguments\n");
+            else if (splitedCommandCount > 3)
+                printf("setenv: too many arguments\n");
+            else
+                setenv(splitedCommand[1], splitedCommand[2], 1);
+            currentCommandIndex += 3;
+        }
+
+        for (int i = currentCommandIndex; i < splitedCommandCount; i++)
+        {
+            /**
+             * Build the command
+             *
+             * Pipe all previous command output to the current command,
+             * it doesn't take effect if there is no previous command output or the current command is not accept input.
+             */
+            char *currentCommand = malloc(MAX_COMMANDS_SIZE * sizeof(char));
+
+            /**
+             * Pipe counter:
+             * 0: no pipe
+             * 1: current command is piped
+             * >1: command output is queued to be piped
+             */
+            pipeCounter = (pipeCounter < 1) ? 0 : pipeCounter - 1;
+
+            while (i < splitedCommandCount && strstr(splitedCommand[i], "|") == NULL)
+            {
+                strcat(currentCommand, splitedCommand[i]);
+                strcat(currentCommand, " ");
+                i++;
+            }
+
+            // printf("currentCommand: %s\n", currentCommand);
+            run_command(currentCommand);
+
+            /**
+             * Check if the command contains pipe
+             * If present, save the output of the command to savedCommandOutput.
+             */
+            if (splitedCommand[i] != NULL)
+            {
+                char *pipCount = strchr(splitedCommand[i], '|');
+                if (pipCount != NULL)
+                {
+                    pipeCounter = (strlen(pipCount) > 1) ? atoi(&pipCount[1]) : 1;
+                    strcpy(savedCommandOutput, previousCommandOutput);
+                    piped = 1;
+                }
+            }
+        }
+
+        /**
+         * If command not found, length of previousCommandOutput will be 0.
+         * If the command is piped, not print to the console
+         * If the next command is the piped command, override the previousCommandOutput with savedCommandOutput
+         *
+         * Pipcounter:
+         * 0: no pipe
+         * 1: next command is piped
+         * >1: ignore
+         */
+        // printf("previousCommandOutput: %d\n", strlen(previousCommandOutput));
+        if (strlen(previousCommandOutput) > 0)
+        {
+            if (pipeCounter == 0)
+            {
+                printf("%s\n", previousCommandOutput);
+                memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+            }
+            if (pipeCounter != 0 && !piped)
+                printf("%s\n", previousCommandOutput);
+            if (pipeCounter == 1)
+            {
+                memset(previousCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+                strcat(previousCommandOutput, savedCommandOutput);
+                memset(savedCommandOutput, 0, PREVIOUS_COMMAND_OUTPUT_SIZE);
+            }
+        }
+        else
+        {
+            // command not found will not consume pipeCounter
+            if (pipeCounter != 0)
+                pipeCounter++;
+        }
+    }
+}
