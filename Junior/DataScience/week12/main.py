5da7cb6ebf1685077bc2e004001747221e2109b3
yumeow0122 <yumeow0122@gmail.com>
2023-05-04 15:45:07 +0800

    DataScience HW12

--- Junior/DataScience/week12/main.py
+++ Junior/DataScience/week12/main.py
@@ -13,6 +13,7 @@ axs[0, 0].plot(df['Township'], df['Total(Male+Female)'], label="Total")
 axs[0, 0].set_title("Population by Gender and Township")
 axs[0, 0].set_xlabel("Township")
 axs[0, 0].set_ylabel("Population")
+axs[0, 0].tick_params(axis='x', labelrotation=60)
 axs[0, 0].legend()
 
 # plot the second subplot: line chart of immigration and emigration
@@ -21,6 +22,7 @@ axs[0, 1].plot(df['Township'], df['Emigration'], label="Emigration")
 axs[0, 1].set_title("Immigration and Emigration by Township")
 axs[0, 1].set_xlabel("Township")
 axs[0, 1].set_ylabel("Number of People")
+axs[0, 1].tick_params(axis='x', labelrotation=60)
 axs[0, 1].legend()
 
 # plot the third subplot: bar chart of household number
@@ -28,6 +30,7 @@ axs[1, 0].bar(df['Township'], df['Number_of_Households'])
 axs[1, 0].set_title("Household Number by Township")
 axs[1, 0].set_xlabel("Township")
 axs[1, 0].set_ylabel("Number of Number_of_Households")
+axs[1, 0].tick_params(axis='x', labelrotation=60)
 
 # plot the fourth subplot: line chart of births and Death
 axs[1, 1].plot(df['Township'], df['Birth'], label="Birth")
@@ -35,10 +38,11 @@ axs[1, 1].plot(df['Township'], df['Death'], label="Death")
 axs[1, 1].set_title("Birth and Death by Township")
 axs[1, 1].set_xlabel("Township")
 axs[1, 1].set_ylabel("Number of People")
+axs[1, 1].tick_params(axis='x', labelrotation=60)
 axs[1, 1].legend()
 
 # adjust the spacing between subplots
-fig.subplots_adjust(hspace=0.4, wspace=0.3)
+fig.subplots_adjust(hspace=1)
 
 # display the plot
 plt.show()
