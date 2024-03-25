532ea790a30594b5634f5d3a2d32fbf1975952f7
yumeow0122 <yumeow0122@gmail.com>
2024-03-25 22:05:37 +0800

    ML: Project initiate

---
+++ Machine Learning/HW1/main.py
@@ -0,0 +1,24 @@
+# PyTorch
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+# For data preprocess
+import numpy as np
+import csv
+import os
+
+# For plotting
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+
+myseed = 2020815  # set a random seed for reproducibility
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(myseed)
+torch.manual_seed(myseed)
+if torch.cuda.is_available():
+    torch.cuda.manual_seed_all(myseed)
+    
+print("PyTorch Version: ", torch.__version__)
+print("Hello World!")
--- Machine Learning/README.md
+++ Machine Learning/README.md
@@ -1,5 +1,8 @@
 # Machine Learning
 
-This section documents are my self-study journey for the NTU ML 2021 course.
+This section documents are my self-study journey for the machine learning.
 
-Course Link: [NTU ML 2021 Lecture Series](https://www.youtube.com/watch?v=Ye018rCVvOo&list=PLJV_el3uVTsMhtt7_Y6sgTHGHp1Vb2P2J&ab_channel=Hung-yiLee)
+Course Link: [NTU ML 2021 Lecture Series](https://speech.ee.ntu.edu.tw/~hylee/ml/2021-spring.php)
+
+Course Link: [NTU ML 2023 Lecture Series](https://speech.ee.ntu.edu.tw/~hylee/ml/2023-spring.php)
+
