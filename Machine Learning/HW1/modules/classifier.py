0f89fef143534de277c8982aabeb234c9559da61
yumeow0122 <yumeow0122@gmail.com>
2024-04-10 16:06:13 +0800

    ML HW1: bn do

--- Machine Learning/HW1/modules/classifier.py
+++ Machine Learning/HW1/modules/classifier.py
@@ -5,11 +5,15 @@ class CovidClassifier(nn.Module):
         super(CovidClassifier, self).__init__()
         # TODO: modify model's structure, be aware of dimensions. 
         self.layers = nn.Sequential(
-            nn.Linear(input_dim, 64),
-            nn.LeakyReLU(0.1)
-            nn.Linear(64, 16),
-            nn.LeakyReLU(0.1)
-            nn.Linear(16, 1)
+            nn.Linear(input_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.5),
+            nn.Linear(256, 64),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(0.1),
+            nn.Dropout(0.5),
+            nn.Linear(64, 1)
         )
 
     def forward(self, x):
